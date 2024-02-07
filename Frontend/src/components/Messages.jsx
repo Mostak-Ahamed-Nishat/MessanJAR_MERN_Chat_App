3ca0fb42f03c7429c343f32fc6a0ba51480e015f
Mostak-Ahamed-Nishat <mostakahamed484@gmail.com>
2024-02-07 17:20:23 +0600

    user inbox component design added

---
+++ Frontend/src/components/Messages.jsx
@@ -0,0 +1,256 @@
+export default function Messages() {
+  return (
+    <div className="message-show">
+      {/* conversation start  */}
+
+      <div className="my-message">
+        <div className="image-message">
+          <div className="my-text">
+            <p className="message-text"> How Are You? </p>
+          </div>
+          <div className="userImage">
+            <img
+              src="https://images.unsplash.com/photo-1522556189639-b150ed9c4330?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="time">2 Jan 2022</div>
+      </div>
+
+      <div className="fd-message">
+        <div className="image-message-time">
+          <img
+            src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+          <div className="message-time">
+            <div className="fd-text">
+              <p className="message-text">I am Fine </p>
+            </div>
+            <div className="time">3 Jan 2022</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="my-message">
+        <div className="image-message">
+          <div className="my-text">
+            <p className="message-text"> What are you doing? </p>
+          </div>
+          <div className="userImage">
+            <img
+              src="https://images.unsplash.com/photo-1522556189639-b150ed9c4330?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="time">2 Jan 2022</div>
+      </div>
+
+      <div className="fd-message">
+        <div className="image-message-time">
+          <img
+            src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+          <div className="message-time">
+            <div className="fd-text">
+              <p className="message-text">
+                Nothing much.Creating a modern chap application. Tell me about
+                u.
+              </p>
+            </div>
+            <div className="time">3 Jan 2022</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="my-message">
+        <div className="image-message">
+          <div className="my-text">
+            <p className="message-text">
+              All good. Busy with my tight work schedule.
+            </p>
+          </div>
+          <div className="userImage">
+            <img
+              src="https://images.unsplash.com/photo-1522556189639-b150ed9c4330?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="time">2 Jan 2022</div>
+      </div>
+
+      <div className="fd-message">
+        <div className="image-message-time">
+          <img
+            src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+          <div className="message-time">
+            <div className="fd-text">
+              <p className="message-text">Let's make a tour plan </p>
+            </div>
+            <div className="time">3 Jan 2022</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="my-message">
+        <div className="image-message">
+          <div className="my-text">
+            <p className="message-text">
+              {" "}
+              Sounds good. Yah could be. Any desire place ?{" "}
+            </p>
+          </div>
+          <div className="userImage">
+            <img
+              src="https://images.unsplash.com/photo-1522556189639-b150ed9c4330?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="time">2 Jan 2022</div>
+      </div>
+
+      <div className="fd-message">
+        <div className="image-message-time">
+          <img
+            src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+          <div className="message-time">
+            <div className="fd-text">
+              <p className="message-text">Nope. You can suggest any.</p>
+            </div>
+            <div className="time">3 Jan 2022</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="my-message">
+        <div className="image-message">
+          <div className="my-text">
+            <p className="message-text">My mind is blank now. have to check.</p>
+          </div>
+          <div className="userImage">
+            <img
+              src="https://images.unsplash.com/photo-1522556189639-b150ed9c4330?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="time">2 Jan 2022</div>
+      </div>
+
+      <div className="fd-message">
+        <div className="image-message-time">
+          <img
+            src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+          <div className="message-time">
+            <div className="fd-text">
+              <p className="message-text">ok. No hurry. let me know later.</p>
+            </div>
+            <div className="time">3 Jan 2022</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="my-message">
+        <div className="image-message">
+          <div className="my-text">
+            <p className="message-text">What about cox's bazar ? </p>
+          </div>
+          <div className="userImage">
+            <img
+              src="https://images.unsplash.com/photo-1522556189639-b150ed9c4330?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="time">2 Jan 2022</div>
+      </div>
+
+      <div className="fd-message">
+        <div className="image-message-time">
+          <img
+            src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+          <div className="message-time">
+            <div className="fd-text">
+              <p className="message-text">MMM no. I been there several times.</p>
+            </div>
+            <div className="time">3 Jan 2022</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="my-message">
+        <div className="image-message">
+          <div className="my-text">
+            <p className="message-text"> do you prefer outside of the country ? </p>
+          </div>
+          <div className="userImage">
+            <img
+              src="https://images.unsplash.com/photo-1522556189639-b150ed9c4330?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="time">2 Jan 2022</div>
+      </div>
+
+      <div className="fd-message">
+        <div className="image-message-time">
+          <img
+            src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+          <div className="message-time">
+            <div className="fd-text">
+              <p className="message-text">Yaahh. I do. but little bit costly. </p>
+            </div>
+            <div className="time">3 Jan 2022</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="my-message">
+        <div className="image-message">
+          <div className="my-text">
+            <p className="message-text"> How Are You? </p>
+          </div>
+          <div className="userImage">
+            <img
+              src="https://images.unsplash.com/photo-1522556189639-b150ed9c4330?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTd8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="time">2 Jan 2022</div>
+      </div>
+
+      <div className="fd-message">
+        <div className="image-message-time">
+          <img
+            src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+          <div className="message-time">
+            <div className="fd-text">
+              <p className="message-text">I am Fine </p>
+            </div>
+            <div className="time">3 Jan 2022</div>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
--- Frontend/src/components/RightSide.jsx
+++ Frontend/src/components/RightSide.jsx
@@ -1,4 +1,6 @@
 import { FaPhoneAlt, FaVideo, FaRocketchat } from "react-icons/fa";
+import Messages from "./Messages";
+import MessageInbox from "./MessageInbox";
 
 export default function Media() {
   return (
@@ -10,7 +12,10 @@ export default function Media() {
               <div className="header">
                 <div className="image-name">
                   <div className="image">
-                    <img src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                    <img
+                      src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                      alt=""
+                    />
                   </div>
                   <div className="name">
                     <h3> GM Sumon </h3>
@@ -31,6 +36,10 @@ export default function Media() {
                   </div>
                 </div>
               </div>
+
+              {/* Message Conversations  */}
+              <Messages />
+              <MessageInbox/>
             </div>
           </div>
 
